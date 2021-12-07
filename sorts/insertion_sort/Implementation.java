0adf8d6eb0900617d17dbaf6c02f87e14082eb33
bietdoikiem <minhlucky2408@gmail.com>
2021-12-07 15:50:35 +0700

    feat: add insertion sort for integer array

--- sorts/insertion_sort/Implementation.java
+++ sorts/insertion_sort/Implementation.java
@@ -1,11 +1,30 @@
 package sorts.insertion_sort;
 
+import java.util.Arrays;
+
 public class Implementation {
   public static void main(String[] args) {
-    // Impl
+    int[] intArr = new int[] { 85, 12, 59, 45, 72, 51 };
+    insertionSort(intArr);
+    System.out.println("Sorted Array:" + Arrays.toString(intArr));
   }
 
+  /**
+   * Insertion Sort the integer array. Time complexity: Worst case O(n^2), Best
+   * Case O(n)
+   * NOTE: We SHIFT the elements not SWAP!
+   * 
+   * @param arr
+   */
   public static void insertionSort(int[] arr) {
-    // NOTE: Impl
+    for (int i = 1; i < arr.length; i++) {
+      int temp = arr[i];
+      int j = i - 1;
+      while (j >= 0 && arr[j] > temp) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = temp;
+    }
   }
 }
